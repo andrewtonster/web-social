7eca9b963a6e558137a2c88bcb92f43be5214651
andrewtonster <andrewtonster@gmail.com>
2024-10-11 18:32:19 -0700

    updated likes

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
 import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
@@ -8,12 +8,41 @@ import { Link } from "react-router-dom";
 import "./post.scss";
 import Comments from "../comments/Comments";
 import moment from "moment";
+import { makeRequest } from "../../axios";
+import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
+import { AuthContext } from "../../context/authContext";
 
 const Post = ({ post }) => {
   const [commentOpen, setCommmentOpen] = useState(false);
+  const { currentUser } = useContext(AuthContext);
 
-  const liked = true;
+  const queryClient = useQueryClient();
 
+  // getting all the user id who liked the post
+  const { isLoading, isError, data, error } = useQuery({
+    queryKey: ["likes", post.id],
+    queryFn: () =>
+      makeRequest.get("/likes?postId=" + post.id).then((res) => res.data),
+  });
+
+  const mutationKey = ["likes"];
+
+  // if current user included in list of likes
+  const mutation = useMutation({
+    mutationKey,
+    mutationFn: (liked) => {
+      if (liked) return makeRequest.delete("/likes?postId=" + post.id);
+      return makeRequest.post("/likes", { postId: post.id });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["likes"]);
+    },
+  });
+
+  // call the mutation function when like button cliked
+  const handleLike = () => {
+    mutation.mutate(data.includes(currentUser.id));
+  };
   return (
     <div className="post">
       <div className="container">
@@ -42,8 +71,17 @@ const Post = ({ post }) => {
         </div>
         <div className="info">
           <div className="item">
-            {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
-            12 likes
+            {isLoading ? (
+              "isloading"
+            ) : data.includes(currentUser.id) ? (
+              <FavoriteOutlinedIcon
+                style={{ color: "red" }}
+                onClick={handleLike}
+              />
+            ) : (
+              <FavoriteBorderOutlinedIcon onClick={handleLike} />
+            )}
+            {data ? data.length : 0} Likes
           </div>
           <div className="item" onClick={() => setCommmentOpen(!commentOpen)}>
             <TextsmsOutlinedIcon />
