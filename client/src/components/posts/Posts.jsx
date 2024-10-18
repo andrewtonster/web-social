ff40c412ad37bd8b55e510738cc39fe3b2c34478
andrewtonster <andrewtonster@gmail.com>
2024-10-18 00:27:25 -0700

    updated update button

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -4,15 +4,23 @@ import Post from "../post/post";
 import { makeRequest } from "../../axios";
 import { useQuery } from "@tanstack/react-query";
 
-const Posts = () => {
+const Posts = ({ userId }) => {
   // we are requesting the data for others post
   // and then returning data
+  // initialize a query key, so when we do invalidate queries we fetch
+  // these posts again and is stored in data
+  // we use cache data (save locally?) so that if the data under posts
+  // doesnt change we do not have to refetch
   const { isLoading, isError, data, error } = useQuery({
     queryKey: ["posts"],
-    queryFn: () => makeRequest.get("/posts").then((res) => res.data),
+    queryFn: () =>
+      makeRequest.get("/posts?userId=" + userId).then((res) => res.data),
   });
 
   console.log(data);
+
+  // if we have not recieved all our posts yet from the query,
+  // then show loading
   return (
     <div className="posts">
       {isError
--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -13,71 +13,113 @@ import Place from "@mui/icons-material/Place";
 import { makeRequest } from "../../axios";
 import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
 import { useLocation } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../context/authContext";
+import Update from "../../components/update/Update";
 
 const Profile = () => {
+  const [openUpdate, setOpenUpdate] = useState(false);
   const { currentUser } = useContext(AuthContext);
-  const userId = useLocation().pathname.split("/")[2];
+
+  const userId = parseInt(useLocation().pathname.split("/")[2]);
+  // console.log("/users/find/" + userId);
+
+  // fetching data recievs time so make sure handle rendering accordingly
   const { isLoading, isError, data, error } = useQuery({
     queryKey: ["user"],
     queryFn: () =>
       makeRequest.get("/users/find/" + userId).then((res) => res.data),
   });
 
-  console.log(data);
+  const { isLoading: rIsLoading, data: relationshipData } = useQuery({
+    queryKey: ["relationship"],
+    queryFn: () =>
+      makeRequest
+        .get("/relationships?followedUserId=" + userId)
+        .then((res) => res.data),
+  });
 
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: (following) => {
+      if (following)
+        return makeRequest.delete("/relationships?userId=" + userId);
+      return makeRequest.post("/relationships", { userId });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["relationship"]);
+    },
+  });
+
+  // call the mutation function when like button cliked
+
+  const handleFollow = () => {
+    mutation.mutate(relationshipData.includes(currentUser.id));
+  };
   return (
-    <div className="profile">
-      <div className="images">
-        <img src={"./friend.png"} alt="" className="cover" />
-        <img src={"./friend.png"} alt="" className="profilePic" />
-      </div>
-      <div className="profileContainer">
-        <div className="uInfo">
-          <div className="left">
-            <a href="http://facebook.com">
-              <FacebookTwoToneIcon fontSize="large" />
-            </a>
-            <a href="http://facebook.com">
-              <InstagramIcon fontSize="large" />
-            </a>
-            <a href="http://facebook.com">
-              <TwitterIcon fontSize="large" />
-            </a>
-            <a href="http://facebook.com">
-              <LinkedInIcon fontSize="large" />
-            </a>
-            <a href="http://facebook.com">
-              <PinterestIcon fontSize="large" />
-            </a>
+    <>
+      {isLoading ? (
+        "loading"
+      ) : (
+        <div className="profile">
+          <div className="images">
+            <img src={"/friend.png"} alt="" className="cover" />
+            <img src={"/friend.png"} alt="" className="profilePic" />
           </div>
-          <div className="center">
-            <span>name</span>
-            <div className="info">
-              <div className="item">
-                <PlaceIcon />
-                <span>USA</span>
+          <div className="profileContainer">
+            <div className="uInfo">
+              <div className="left">
+                <a href="http://facebook.com">
+                  <FacebookTwoToneIcon fontSize="large" />
+                </a>
+                <a href="http://facebook.com">
+                  <InstagramIcon fontSize="large" />
+                </a>
+                <a href="http://facebook.com">
+                  <TwitterIcon fontSize="large" />
+                </a>
+                <a href="http://facebook.com">
+                  <LinkedInIcon fontSize="large" />
+                </a>
+                <a href="http://facebook.com">
+                  <PinterestIcon fontSize="large" />
+                </a>
               </div>
-              <div className="item">
-                <LanguageIcon />
-                <span>website</span>
+              <div className="center">
+                <span>{data && data.name}</span>
+                <div className="info">
+                  <div className="item">
+                    <PlaceIcon />
+                    <span>USA</span>
+                  </div>
+                  <div className="item">
+                    <LanguageIcon />
+                    <span>website</span>
+                  </div>
+                </div>
+                {rIsLoading ? (
+                  "loading"
+                ) : userId === currentUser.id ? (
+                  <button onClick={() => setOpenUpdate(true)}>Update</button>
+                ) : (
+                  <button onClick={handleFollow}>
+                    {relationshipData.includes(currentUser.id)
+                      ? "Following"
+                      : "Follow"}
+                  </button>
+                )}
+              </div>
+              <div className="right">
+                <EmailOutlinedIcon />
+                <MoreVertIcon />
               </div>
             </div>
-            {userId === currentUser.id ? (
-              <button>Update</button>
-            ) : (
-              <button>follow</button>
-            )}
-          </div>
-          <div className="right">
-            <EmailOutlinedIcon />
-            <MoreVertIcon />
+            <Posts userId={userId} />
           </div>
         </div>
-        <Posts />
-      </div>
-    </div>
+      )}
+      {openUpdate && <Update setOpenUpdate={setOpenUpdate} user={data} />}
+    </>
   );
 };
 
